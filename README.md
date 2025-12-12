25ba12b37cc055a8c16fe91bb7ccf3da249bd86b
Raj-StepSecurity <rajkumar@stepsecurity.io>
2025-12-12 12:31:32 +0530

    feat: onboarding setup-android action
    subscription check added
    
    retired envs removed from workflow
    
    workflow updated
    
    unused deps removed
    
    unused deps removed
    
    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # setup-android
+
+![Build & Test](https://github.com/step-security/setup-android/workflows/Build%20&%20Test/badge.svg)
+
+This action sets up the Android SDK tools by:
+ - Downloading the SDK commandline tools, if the current version (16.0) is not found in either `$ANDROID_SDK_ROOT` or `$HOME/.android/sdk`.
+ - Accepting the SDK licenses.
+ - Installing `tools` and `platform-tools`.
+ - Adding `platform-tools` (contains adb) and `cmdline-tools/16.0/bin` (contains sdkmanager) to `$PATH`.
+ - Setting up problem [matchers](/matchers.json).
+
+On Windows 2016 runners, this action also checks if `$ANDROID_SDK_ROOT` path contains spaces.
+If it does - it moves SDK to a path without spaces. This is needed because spaces are highly problematic:
+```
+C:\windows\system32\cmd.exe /D /S /C ""C:\Program Files (x86)\Android\android-sdk\cmdline-tools\3.0\bin\sdkmanager.bat" --licenses"
+Error: Could not find or load main class Files
+```
+
+# Usage
+
+See [action.yml](action.yml)
+
+## Basic
+```yaml
+steps:
+- uses: actions/checkout@v6
+
+- name: Set up JDK 17
+  uses: actions/setup-java@v4
+  with:
+    java-version: '17'
+    distribution: 'temurin'
+
+- name: Setup Android SDK
+  uses: step-security/setup-android@v3
+
+- name: Build SampleApplication
+  run: ./gradlew --no-daemon build
+```
+
+## Additional packages
+Input parameter `packages` controls which packages this action will install from Android SDK.
+
+Default value is `tools platform-tools`, supply an empty string to skip installing additional packages.
+
+Additional packages can be installed at a later time by calling sdkmanager manually.
+
+```yaml
+- name: Setup Android SDK
+  uses: step-security/setup-android@v3
+  with:
+    packages: ''
+
+# ...
+
+- run: sdkmanager tools platform-tools
+```
+
+## SDK Version selection
+
+Command line tools are versioned using two variables - short and long.
+Long one is the build number, used in the zip URL, short one is the human friendly version name.
+
+By default, setup-android installs version 12266719 (short version 16.0).
+
+To install a different version, call setup-android with desired long version as the input parameter `cmdline-tools-version`:
+```yaml
+- name: Setup Android SDK
+  uses: step-security/setup-android@v3
+  with:
+    cmdline-tools-version: 8512546
+```
+
+#### Version table
+| Short version | Long version |
+|---------------| --- |
+| 16.0          | 12266719 |
+| 13.0          | 11479570 |
+| 12.0          | 11076708 |
+| 11.0          | 10406996 |
+| 10.0          | 9862592 |
+| 9.0           | 9477386 |
+| 8.0           | 9123335 |
+| 7.0           | 8512546 |
+
+Current cmdline tools version can be found at https://developer.android.com/studio#command-line-tools-only
+
+
+# Android SDK Licenses
+
+Android SDK is not public domain software, it comes with a license.
+
+Input parameter `accept-android-sdk-licenses` decides if Android SDK licenses should be agreed to on behalf of the user of this action.
+Default option is 'yes', because otherwise SDK is unusable until said licenses are agreed to.
+
+licenses are quite long, to prevent a wall of text in the action output, licenses can be agreed to silently.
+Input parameter `log-accepted-android-sdk-licenses` controls whether license texts should be printed or omitted from the text output. Defaults to 'true'.
+
---
+++ src/main.ts
@@ -0,0 +1,211 @@
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import axios, {isAxiosError} from 'axios'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as os from 'os'
+
+function getVersionShort(versionLong: string): string {
+  switch (versionLong) {
+    case '12266719':
+      return '16.0'
+    case '11479570':
+      return '13.0'
+    case '11076708':
+      return '12.0'
+    case '10406996':
+      return '11.0'
+    case '9862592':
+      return '10.0'
+    case '9477386':
+      return '9.0'
+    case '9123335':
+      return '8.0'
+    case '8512546':
+      return '7.0'
+    default:
+      return versionLong
+  }
+}
+
+const VERSION_LONG = core.getInput('cmdline-tools-version', {
+  trimWhitespace: true
+})
+if (VERSION_LONG.includes('/') || VERSION_LONG.includes('\\')) {
+  core.setFailed('Malformed cmdline-tools-version!')
+  throw new Error('Malformed cmdline-tools-version!')
+}
+const VERSION_SHORT = getVersionShort(VERSION_LONG)
+
+const COMMANDLINE_TOOLS_WIN_URL = `https://dl.google.com/android/repository/commandlinetools-win-${VERSION_LONG}_latest.zip`
+const COMMANDLINE_TOOLS_MAC_URL = `https://dl.google.com/android/repository/commandlinetools-mac-${VERSION_LONG}_latest.zip`
+const COMMANDLINE_TOOLS_LIN_URL = `https://dl.google.com/android/repository/commandlinetools-linux-${VERSION_LONG}_latest.zip`
+
+const ANDROID_HOME_SDK_DIR = path.join(os.homedir(), '.android', 'sdk')
+let ANDROID_SDK_ROOT = process.env['ANDROID_SDK_ROOT'] || ANDROID_HOME_SDK_DIR
+
+async function callSdkManager(
+  sdkManager: string,
+  arg: string,
+  printOutput: Boolean = true
+): Promise<void> {
+  const acceptBuffer = Buffer.from(Array(10).fill('y').join('\n'), 'utf8')
+  await exec.exec(sdkManager, [arg], {
+    input: acceptBuffer,
+    silent: !printOutput
+  })
+}
+
+async function installSdkManager(): Promise<string> {
+  const cmdlineTools = path.join(
+    ANDROID_SDK_ROOT,
+    'cmdline-tools',
+    VERSION_SHORT
+  )
+  let sdkManagerExe = path.join(cmdlineTools, 'bin', 'sdkmanager')
+
+  if (!fs.existsSync(sdkManagerExe)) {
+    const latestCmdlineTools = path.join(
+      ANDROID_SDK_ROOT,
+      'cmdline-tools',
+      'latest'
+    )
+    const sourcePropertiesFile = path.join(
+      latestCmdlineTools,
+      'source.properties'
+    )
+    const latestSdkManagerExe = path.join(
+      latestCmdlineTools,
+      'bin',
+      'sdkmanager'
+    )
+    if (
+      fs.existsSync(latestCmdlineTools) &&
+      fs.existsSync(sourcePropertiesFile) &&
+      fs.existsSync(latestSdkManagerExe)
+    ) {
+      const sourceProperties = fs.readFileSync(sourcePropertiesFile)
+      core.info(
+        `Found preinstalled sdkmanager in ${latestCmdlineTools} with following source.properties:`
+      )
+      core.info(sourceProperties.toString())
+      if (sourceProperties.includes(`Pkg.Revision=${VERSION_SHORT}`)) {
+        core.info(`Preinstalled sdkmanager has the correct version`)
+        sdkManagerExe = latestSdkManagerExe
+      } else {
+        core.info(`Wrong version in preinstalled sdkmanager`)
+      }
+    }
+  }
+
+  if (!fs.existsSync(sdkManagerExe)) {
+    let cmdlineToolsURL
+    if (process.platform === 'linux') {
+      cmdlineToolsURL = COMMANDLINE_TOOLS_LIN_URL
+    } else if (process.platform === 'darwin') {
+      cmdlineToolsURL = COMMANDLINE_TOOLS_MAC_URL
+    } else if (process.platform === 'win32') {
+      cmdlineToolsURL = COMMANDLINE_TOOLS_WIN_URL
+    } else {
+      core.error(`Unsupported platform: ${process.platform}`)
+      return ''
+    }
+
+    core.info(`Downloading commandline tools from ${cmdlineToolsURL}`)
+    const cmdlineToolsZip = await tc.downloadTool(cmdlineToolsURL)
+
+    const extractTo = path.join(ANDROID_SDK_ROOT, 'cmdline-tools')
+    await tc.extractZip(cmdlineToolsZip, extractTo)
+
+    // Make sure we don't have leftover target directory (happens sometimes...)
+    if (fs.existsSync(cmdlineTools)) {
+      core.info(`Removing leftovers from ${cmdlineTools}`)
+      fs.rmSync(cmdlineTools, {recursive: true})
+    }
+    fs.renameSync(path.join(extractTo, 'cmdline-tools'), cmdlineTools)
+  }
+
+  // touch $ANDROID_SDK_ROOT/repositories.cfg
+  fs.closeSync(
+    fs.openSync(path.join(ANDROID_SDK_ROOT, 'repositories.cfg'), 'w')
+  )
+  core.debug(`sdkmanager available at: ${sdkManagerExe}`)
+  return sdkManagerExe
+}
+
+async function validateSubscription(): Promise<void> {
+  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`
+
+  try {
+    await axios.get(API_URL, {timeout: 3000})
+  } catch (err) {
+    if (isAxiosError(err) && err.response?.status === 403) {
+      core.error(
+        'Subscription is not valid. Reach out to support@stepsecurity.io'
+      )
+      process.exit(1)
+    } else {
+      core.info('Timeout or API not reachable. Continuing to next step.')
+    }
+  }
+}
+
+async function run(): Promise<void> {
+  await validateSubscription()
+  if ('win16' === process.env['ImageOS']) {
+    if (-1 !== ANDROID_SDK_ROOT.indexOf(' ')) {
+      // On Windows2016, Android SDK is installed to Program Files,
+      // and it doesn't really work..
+      // C:\windows\system32\cmd.exe /D /S /C ""C:\Program Files (x86)\Android\android-sdk\cmdline-tools\3.0\bin\sdkmanager.bat" --licenses"
+      // Error: Could not find or load main class Files
+
+      const newSDKLocation = ANDROID_SDK_ROOT.replace(/\s/gi, '-')
+      core.debug(`moving ${ANDROID_SDK_ROOT} to ${newSDKLocation}`)
+      fs.mkdirSync(path.dirname(newSDKLocation), {recursive: true})
+
+      // intentionally using fs.renameSync,
+      // because it doesn't move across drives
+      fs.renameSync(ANDROID_SDK_ROOT, newSDKLocation)
+      ANDROID_SDK_ROOT = newSDKLocation
+    }
+  }
+
+  const sdkManagerExe = await installSdkManager()
+
+  if (core.getBooleanInput('accept-android-sdk-licenses')) {
+    core.info('Accepting Android SDK licenses')
+    await callSdkManager(
+      sdkManagerExe,
+      '--licenses',
+      core.getBooleanInput('log-accepted-android-sdk-licenses')
+    )
+  }
+
+  const packages = core
+    .getInput('packages', {required: false})
+    .split(' ')
+    .map(function (str) {
+      return str.trim()
+    })
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+    .filter(function (element, index, array) {
+      return element
+    })
+  for (const pkg of packages) {
+    await callSdkManager(sdkManagerExe, pkg)
+  }
+
+  core.setOutput('ANDROID_COMMANDLINE_TOOLS_VERSION', VERSION_LONG)
+  core.exportVariable('ANDROID_HOME', ANDROID_SDK_ROOT)
+  core.exportVariable('ANDROID_SDK_ROOT', ANDROID_SDK_ROOT)
+
+  core.addPath(path.dirname(sdkManagerExe))
+  core.addPath(path.join(ANDROID_SDK_ROOT, 'platform-tools'))
+
+  core.debug('add matchers')
+  // eslint-disable-next-line no-console
+  console.log(`##[add-matcher]${path.join(__dirname, '..', 'matchers.json')}`)
+}
+
+run()
